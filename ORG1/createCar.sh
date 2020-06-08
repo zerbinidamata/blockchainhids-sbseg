f250089d600fcdadeb712f5a72d1f484bff16dbe
zerbinidamata <rafael.zerbinia@gmail.com>
2020-06-08 20:31:17 -0300

    Rules model done + readme

--- ORG1/createCar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CHAINCODE_NAME="fabcar"
-CHAINCODE_SRC="/opt/gopath/src/github.com/chaincode/fabcar/node"
-CHAINCODE_VERSION="1.0.0"
-CHANNEL_NAME="mychannel"
-
-docker exec Org1-cli peer chaincode invoke -o orderer.example.com:7050 -C $CHANNEL_NAME -n $CHAINCODE_NAME -c '{"Args":["createCar", "CAR3", "Toyata", "Invoa", "Black", "Sheldon"]}'
